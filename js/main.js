d4622d486612c6e2e731cb743bcebb65d5f3971b
Aaron Sullivan <aasullivan1618@gmail.com>
2013-12-25 10:35:35 -0500

    finishing up population of main stuff

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
-$(document).ready(function(){
-	
-	// The official Timer, woohoo!
-	window.getRemainingSeconds = function() {
+var Countdown = this.Countdown = (this.Countdown || {});
+
+(function(global){ // Countdown Timer
+	var CountdownTimer = global.CountdownTimer = function($element){
+		this.$element = $element;
+		this.accuracy = 100; // milliseconds per update time
+		this.initialize();
+	};
+
+	CountdownTimer.prototype.getRemainingSeconds = function() {
 		return ((new Date(2050, 1, 1, 4, 20, 00).valueOf() / 1000) - Math.floor(new Date().valueOf() / 1000)) % (60 * 60 * 12)
 	};
 
-	window.translateTime = function(givenSeconds) {
+	CountdownTimer.prototype.translateTime = function(givenSeconds) {
 		var hours = Math.floor(givenSeconds / 3600);
 		var minutes = Math.floor((givenSeconds / 60) % 60);
 		var seconds = givenSeconds % 60;
@@ -23,10 +29,25 @@ $(document).ready(function(){
 		var timeString = timeArray[0] + ":" + timeArray[1] + ":" + timeArray[2];
 
 		return timeString; 
-	}
+	};
 
-	window.countdownTimer = setInterval(function(){
-		console.log(translateTime(getRemainingSeconds()));
-	}, 300)
+	CountdownTimer.prototype.replaceTime = function (newTime) {
+		this.$element.html(newTime);
+	};
 
+	CountdownTimer.prototype.step = function(){
+		var remainingSeconds = this.getRemainingSeconds();
+		var timeString = this.translateTime(remainingSeconds);
+		this.replaceTime(timeString);
+	};
+
+	CountdownTimer.prototype.initialize = function(){
+		var that = this;
+		window.countdownTimer = setInterval(that.step.bind(that), that.accuracy);
+	};
+})(Countdown)
+
+
+$(document).ready(function(){
+	new Countdown.CountdownTimer($('#countdown-wrapper'));
 })
