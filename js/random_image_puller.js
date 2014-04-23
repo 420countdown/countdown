f54331fd0b90ef9aa4340d014834078917912c15
Aaron Sullivan <aasullivan1618@gmail.com>
2014-04-22 21:41:37 -0700

    adds "hover over image prevents image change" functionality

--- js/random_image_puller.js
+++ js/random_image_puller.js
@@ -2,14 +2,32 @@ var Countdown = this.Countdown = (this.Countdown || {});
 
 (function(global){
 	var RandomImagePuller = global.RandomImagePuller = function () {
-		this.elements = [].slice.call(arguments, 0);
+		this.$elements = [].slice.call(arguments, 0);
 		this.fadeDuration = 400;
-		// this.initialize();
 		this.imageChangeDuration = 2500;
 		this.imageSources = [
 			'http://concentrate-marijuana-420.jpg.to/r?',
 			'http://420-weed.jpg.to/r?'
 		]
+		this._assignHoverListenersToEls();
+		// this.initialize();
+	};
+
+	RandomImagePuller.prototype._assignHoverListenersToEls = function() {
+		this.$elements.forEach(function ($el) {
+			$el.hover(
+				function (e) {
+					$el.toggleClass('hovered', true);
+				},
+				function (e) {
+					$el.toggleClass('hovered', false);
+				}
+			)
+		})
+	};
+
+	RandomImagePuller.prototype.canChange = function ($el) {
+		return !$el.hasClass('hovered');
 	};
 
 	RandomImagePuller.prototype._random = function (array) {
@@ -40,13 +58,17 @@ var Countdown = this.Countdown = (this.Countdown || {});
 
 	RandomImagePuller.prototype.initialPopulation = function(){
 		var that = this;
-		this.elements.forEach(function (el) {
+		this.$elements.forEach(function (el) {
 			that.assignImage(el);
 		})
 	};
 
 	RandomImagePuller.prototype.step = function(){
-		var $el = this._random(this.elements)
+		var that = this;
+		var $el = this._random(this.$elements);
+		while(!that.canChange($el)){
+			$el = that._random(that.$elements);
+		}
 		this.assignImage($el);
 	};
 
