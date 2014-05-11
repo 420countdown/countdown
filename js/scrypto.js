0ccf640e82e0f16bf633dd06f402cb5aa9104c46
420CountDown <420countdown@users.noreply.github.com>
2014-05-11 16:16:37 -0700

    pulled out scripts from html & put into unique file

---
+++ js/scrypto.js
@@ -0,0 +1,30 @@
+
+  <!-- UserVoice JavaScript SDK (only needed once on a page) -->
+(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/cEePMdC92H244qgHc0wNA.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})()
+
+<!-- A tab to launch the Classic Widget -->
+
+UserVoice = window.UserVoice || [];
+UserVoice.push(['showTab', 'classic_widget', {
+  mode: 'feedback',
+  primary_color: '#7db12e',
+  link_color: '#007dbf',
+  forum_id: 250216,
+  tab_label: 'Give us Feedback!!',
+  tab_color: '#7db12e',
+  tab_position: 'middle-left',
+  tab_inverted: true
+}]);
+
+
+<!-- Start of Google Tracking Code -->
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-16011008-8', '420countdown.com');
+  ga('send', 'pageview');
+
+	<!-- End of Google Tracking Code -->
